f7ac6a05ab96cee7e457e14a5469bed893e90240
PurgatoryMP <103337537+PurgatoryMP@users.noreply.github.com>
2023-06-20 02:01:14 -0400

    Created package file
    Created package file.

---
+++ package.py
@@ -0,0 +1,18 @@
+name = "sprite_sheet_creator"
+
+version = "1.0.0"
+
+description = "This tool is for creating sprite sheets from image sequences."
+
+authors = [
+    "DantiaGothly",
+]
+requires = [
+    "PIL",
+    "QT5",
+]
+
+_root_items_to_build = [
+    "assets",
+    "README.txt",
+]
