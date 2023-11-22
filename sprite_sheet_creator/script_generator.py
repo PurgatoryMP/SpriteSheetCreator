f2abd40e3070496631984a4a43e1e11f0d3a52d2
PurgatoryMP <103337537+PurgatoryMP@users.noreply.github.com>
2023-11-21 19:44:05 -0500

    Update script_generator.py
    Simplified the lsl script for the single sprite sheet animation.

--- sprite_sheet_creator/script_generator.py
+++ sprite_sheet_creator/script_generator.py
@@ -183,88 +183,23 @@ public class SpriteSheetAnimator : MonoBehaviour
         """
         Generates a lsl script with all the required parameters.
         """
-        script = """integer animOn = TRUE; //Set to FALSE and call initAnim() again to stop the animation.
-//Effect parameters: (can be put in list together, to make animation have all of said effects)
-//LOOP - loops the animation
-//SMOOTH - plays animation smoothly
-//REVERSE - plays animation in reverse
-//PING_PONG - plays animation in one direction, then cycles in the opposite direction
-list effects = [LOOP];
-integer movement = 0;
-integer link_number = 2;//Number representing the link number that this animation will be played on.
+        script = """integer link_number = 0;//Number representing the link number that this animation will be played on.
 integer face = ALL_SIDES; //Number representing the side to activate the animation on.
 integer sideX = 1; //Represents how many horizontal images (frames) are contained in your texture.
 integer sideY = 1; //Same as sideX, except represents vertical images (frames).
-float start = 0.0; //Frame to start animation on. (0 to start at the first frame of the texture)
-float length = 0.0; //Number of frames to animate, set to 0 to animate all frames.
-float speed = 10.0; //Frames per second to play.
-
-reset()
-{
-    llSetLinkTextureAnim(link_number,FALSE, face, 0, 0, 0.0, 0.0, 1.0); // V2 - remove all anims
-}
-
-animate()
-{
-    if(animOn)
-    {
-        integer effectBits;
-        integer i;
-        for(i = 0; i < llGetListLength(effects); i++)
-        {
-            effectBits = (effectBits | llList2Integer(effects,i));
-        }
-        integer params = (effectBits|movement);
-        llSetLinkTextureAnim(link_number, ANIM_ON|params,face,sideX,sideY,     start,length,speed);
-    }
-    else
-    {
-        llSetLinkTextureAnim(link_number,0,face,sideX,sideY, start,length,speed);
-    }
-}
-
-fetch()
-{
-    string texture = llGetInventoryName(INVENTORY_TEXTURE,0);
-    llSetLinkTexture(link_number,texture,face);
-
-    list data  = llParseString2List(texture,["_"],[]);
-
-    llOwnerSay( llDumpList2String(data ,","));
-
-    string anim_state = llList2String(data,0);
-    string sprite_sheet_number = llList2String(data,1);
-    string rows = llList2String(data,2);
-    string columns = llList2String(data,3);
-    string frame_count = llList2String(data,4);
-    string frame_rate = llList2String(data,5);
-
-    // llOwnerSay("X=" + X + " Y=" + Y + " Z = " + (string) Z);
-
-    sideX = (integer) rows;
-    sideY = (integer) columns;
-    speed = (float) frame_rate;
-
-    if(speed)
-    {
-        animate();
-    }
-}
+float start_frame = 0.0; //Frame to start animation on. (0 to start at the first frame of the texture)
+float end_frame = 64.0; //Number of frames to animate, set to 0 to animate all frames.
+float fps = 24.0; //Frames per second to play.
 
 default
 {
     state_entry()
     {
-        reset();
-        fetch();
-    }
-    changed(integer change)
-    {
-        if (change & CHANGED_INVENTORY)
-        {
-            reset();
-            fetch();
-        }
+        // Stop animated texture.
+        llSetLinkTextureAnim(link_number, FALSE, face, 0, 0, 0.0, 0.0, 1.0);
+        
+        // Loop a single sprite sheet.
+        llSetLinkTextureAnim(link_number, ANIM_ON | LOOP, face, sideX, sideY, start_frame, end_frame, fps );
     }
 }       
         """
